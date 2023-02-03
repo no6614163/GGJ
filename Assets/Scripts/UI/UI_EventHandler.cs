d71fd6dea9aaf1ea7653244bc6a2a9e5354d26b2
no6614163 <103178685+no6614163@users.noreply.github.com>
2023-02-03 21:49:41 +0900

    - UI Scene, Eventhandler 추가

---
+++ Assets/Scripts/UI/UI_EventHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UI_EventHandler : MonoBehaviour, IPointerClickHandler, IDragHandler, IPointerEnterHandler, IPointerExitHandler
+{
+	public Action<PointerEventData> OnClickHandler = null;
+	public Action<PointerEventData> OnDragHandler = null;
+	public Action<PointerEventData> OnEnterHandler = null;
+	public Action<PointerEventData> OnExitHandler = null;
+
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		if (OnClickHandler != null)
+			OnClickHandler.Invoke(eventData);
+	}
+
+	public void OnDrag(PointerEventData eventData)
+	{
+		if (OnDragHandler != null)
+			OnDragHandler.Invoke(eventData);
+	}
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+		if (OnEnterHandler != null)
+			OnEnterHandler.Invoke(eventData);
+	}
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+		if (OnExitHandler != null)
+			OnExitHandler.Invoke(eventData);
+	}
+
+    
+}
